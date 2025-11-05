97201fa150881bac93352c9aad8a982aff77593b
Ritsu <s1f102503183@iniad.org>
2025-11-06 01:12:58 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use bevy::prelude::*;
+
+fn hello_world() {
+    println!("hello world");
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Ritsu_wada".to_string())));
+    commands.spawn((Person, Name("R3".to_string())));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("hello {}", name.0);
+    }
+}
+
+fn main() {
+    App::new()
+        .add_systems(Startup, add_people)
+        .add_systems(Update, (hello_world, greet_people))
+        .run();
+}
